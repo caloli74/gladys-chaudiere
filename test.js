43729062057fdeaee3a2c66676226f5220f8157f
caloli74 <36608702+caloli74@users.noreply.github.com>
2018-03-22 16:10:01 +0100

    commit

--- test.js
+++ test.js
@@ -2,5 +2,7 @@ var fs = require('fs');
 var queries = require('./queries.js');
 
 //gladys.utils.sql(queries.create_tables);
-var param = JSON.parse(fs.readFileSync('param.json');
-console.log(param.schedule[0].day);
+var param = JSON.parse(fs.readFileSync('param.json'));
+param.schedule.forEach((sched) => {
+        console.log(sched.day + ' ' + sched.special_day + ' ' + sched.start_time + ' ' + sched.stop_time);
+});
