6bedcc14d1bfd3ce610f8b4f556d5b98d9949f1c
unknown <caloli1974@gmail.com>
2018-03-26 23:13:58 +0200

    dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (sails) {
     var install = require('./lib/install.js');
     var daemon = require('./lib/daemon.js');
 
-    gladys.on('ready', function(){
+    gladys.on('GatewayReady', function(){
         daemon();
     });
 
