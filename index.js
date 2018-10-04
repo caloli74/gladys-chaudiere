83e6f9e179f8d0bcf767affaf3dad6d40fe34b18
caloli74 <caloli1974@gmail.com>
2018-10-04 23:41:04 +0200

    dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (sails) {
     var install = require('./lib/install.js');
     var daemon = require('./lib/daemon.js');
 
-    gladys.on('GatewayReady', function(){
+    gladys.on('XiaomiReady', function(){
         daemon();
     });
 
